459717cabb1d58622c533fa973e5500cf6312826
Aninha <aninha.clara030807@gmail.com>
2025-04-16 11:56:40 -0300

    Adicionar arquivos de projeto.

---
+++ APIDevSteamJau/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace APIDevSteamJau.Models
+{
+    public class Usuario : IdentityUser
+    {
+        public Usuario() : base()
+        { }
+
+        public string? NomeCompleto { get; set; }
+        public DateOnly DataNascimento { get; set; }
+    }
+}
